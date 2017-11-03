2d9bf33e49aa24528bf06678eadf3a46a5ea9c43
tijs <tijs.fertinel.u8681@student.hogent.be>
2017-11-03 15:29:57 +0100

    challange completed with constraint layouts

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -1,13 +1,43 @@
 package com.londonappbrewery.magiceightball;
 
+import android.content.res.TypedArray;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button askButton;
+    private ImageView eightBall;
+
+    private Random r;
+
+    private TypedArray balls;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        askButton = (Button) findViewById(R.id.ask_button);
+
+        eightBall = (ImageView) findViewById(R.id.image_eight_ball);
+
+        r = new Random();
+
+        balls = getResources().obtainTypedArray(R.array.eight_ball);
+
+        askButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                eightBall.setImageDrawable(balls.getDrawable(r.nextInt(5)));
+
+            }
+        });
     }
 }
